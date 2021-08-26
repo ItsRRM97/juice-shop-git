0d3138d7a8c071d0a68db044fbec1ad2c13b99d8
JuiceShopBot <61591748+JuiceShopBot@users.noreply.github.com>
2021-08-26 03:58:50 +0000

    Auto-fix linting issues
    Signed-off-by: JuiceShopBot <61591748+JuiceShopBot@users.noreply.github.com>

--- app.ts
+++ app.ts
@@ -2,7 +2,7 @@
  * Copyright (c) 2014-2021 Bjoern Kimminich.
  * SPDX-License-Identifier: MIT
  */
-//Added a Comment
+// Added a Comment
 require('./lib/startup/validateDependencies')().then(() => {
   const server = require('./server')
   server.start()
